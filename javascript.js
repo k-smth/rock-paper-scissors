4e424b6350cabf20245ebc34803e662260b10962
snackpacks <trvsh99@gmail.com>
2022-10-25 16:45:38 -0500

    added func to disable buttons when game ends & combined playround func with roundcount & gameresult funcs - now displays results after each selec

--- javascript.js
+++ javascript.js
@@ -1,73 +1,59 @@
-// let playerSelection = '';
-
 let playerScore = 0;
 let computerScore = 0;
 let draws = 0;
+const buttons = document.querySelectorAll('.button');
 
 
 function getComputerChoice() {
     const choice = ['rock', 'paper', 'scissors'];
     return choice[Math.floor(Math.random() * choice.length)];
-}
+};
+
+function disableButtons() {
+    buttons.forEach(e => {
+        e.disabled = true;
+    })
+};
 
 
 function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
+    let results = '';
     console.log(computerSelection); 
 
     if((playerSelection === 'rock' && computerSelection === 'scissors') ||
        (playerSelection === 'paper' && computerSelection === 'rock') ||
        (playerSelection === 'scissors' && computerSelection === 'paper')) {
-            return `You win! ${playerSelection} beats ${computerSelection}!`; 
+            playerScore += 1;
+            results = `You win! ${playerSelection} beats ${computerSelection}!
+            <br><br>Player Score: ${playerScore} Computer Score: ${computerScore}`;
+
+            if(playerScore == 5) {
+                results += `<br>You won the game! Refresh the page and play again!`
+                disableButtons();
+            }
 
     } else if (playerSelection === computerSelection) {
-        return `It is a tie! Both users chose ${playerSelection}!`;
+        draws += 1;
+        results = `It is a tie! Both users chose ${playerSelection}!
+        <br><br>Player Score: ${playerScore} Computer Score: ${computerScore}`;
 
     } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}!`;
-    }
+        computerScore += 1;
+        results = `You lose! ${computerSelection} beats ${playerSelection}!
+        <br><br>Player Score: ${playerScore} Computer Score: ${computerScore}`;
 
+        if(computerScore == 5) {
+            results += `<br>The computer won the game! Refresh the page to play again!`
+            disableButtons();
+        }
+    }
+    document.getElementById('results').innerHTML = results
+    return
 }
 
-const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => {
     button.addEventListener('click', function(){   
       console.log(playRound(button.value));
     })
 });
-
-
-/* function game() {
-   for (let i = 0; i < 100; i++) {
-       //let playerSelection = '';
-       const computerSelection = getComputerChoice();
-       let roundResult = playRound(playerSelection, computerSelection);
-       console.log(roundResult);
-        gameScore(roundResult);
-       console.log('Your score is ' + playerScore + '!');
-       console.log("Computer's score is " + computerScore + '!');
-
-       if (playerScore === 5 || computerScore === 5)
-       break; 
-   }
-}; 
-
- function gameScore(roundResult) {
-   if (roundResult === 'Computer wins! Paper beats rock.' || roundResult === 'Computer wins! Scissors beats paper.' || roundResult === 'Computer wins! Rock beats scissors.') {
-       computerScore++;
-   } else if (roundResult === 'You win! Rock beats scissors.' || roundResult === 'You win! Paper beats rock.' || roundResult === 'You win! Scissors beat paper.') {
-       playerScore++;
-   } else {
-       draws++;
-   }
-
-   if (playerScore === 5) {
-       console.log('You are the winner!')
-       return;
-   };
-
-   if (computerScore === 5) {
-       console.log('The computer is the winner!')
-       return;
-   }
-}; */
